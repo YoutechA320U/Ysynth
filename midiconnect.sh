43153855b5e1d2827388d8bad6524b8cf449dd9b
YoutechA320U <42425276+YoutechA320U@users.noreply.github.com>
2019-01-12 22:11:00 +0900

    Update midiconnect.sh

--- midiconnect.sh
+++ midiconnect.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
+aconnect 14:0 20:0
+aconnect 14:0 24:0
+aconnect 14:0 129:0
+aconnect 14:0 130:0
+
 aconnect 20:0 129:0
+aconnect 20:0 130:0
+
 aconnect 24:0 129:0
-aconnect 128:0 129:0
-aconnect 128:0 24:0
+aconnect 24:0 130:0
+
 aconnect 128:0 20:0
+aconnect 128:0 24:0
+aconnect 128:0 129:0
+
+
