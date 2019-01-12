5d675838dd4d75318c6a2ef79475cbf54301dd62
YoutechA320U <42425276+YoutechA320U@users.noreply.github.com>
2019-01-13 01:13:14 +0900

    Update ysynth.py

--- ysynth.py
+++ ysynth.py
@@ -136,10 +136,9 @@ if sf2 != cfg:
  for x in range(sf2quant):
   subprocess.call( '/home/pi/cfgforsf -C /home/pi/sf2/{}.sf2 /home/pi/timidity_cfg/tmp.tmp' .format(sf2[x])  ,shell=True)
   subprocess.call( "sed -e 's/^[ ]*//g' -e '/(null)/d' -e '/^$/d' -e /^#/d /home/pi/timidity_cfg/tmp.tmp > /home/pi/timidity_cfg/{}.cfg" .format(sf2[x])  ,shell=True)
-  subprocess.call('rm /home/pi/timidity_cfg/*.tmp' ,shell=True)
   subprocess.call('find /home/pi/timidity_cfg -empty -delete' ,shell=True)
   cfg = subprocess.check_output('ls /home/pi/timidity_cfg/*.cfg' ,shell=True).decode('utf-8').strip().replace('/home/pi/timidity_cfg/', '').replace('.cfg', '').split('\n')
-
+ subprocess.call('rm /home/pi/timidity_cfg/*.tmp' ,shell=True)
 if sf2 == cfg:
    time.sleep(4.0)
 midiout.open_virtual_port("Ysynth_out") # 仮想MIDIポートの名前
