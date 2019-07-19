600796c908a24ed8bcf6822337f43c1b6cf7f5c1
YoutechA320U <42425276+YoutechA320U@users.noreply.github.com>
2019-07-20 00:28:43 +0900

    Create midiconnect02.sh

---
+++ midiconnect02.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#aconnect 14:0 20:0
+#aconnect 128:0 20:0
+#aconnect 131:0 20:0
+#aconnect 14:0 24:0
+#aconnect 128:0 24:0
+#aconnect 131:0 24:0
+#aconnect 14:0 129:0
+#aconnect 20:0 129:0
+#aconnect 24:0 129:0
+aconnect 14:0 130:0
+aconnect 20:0 130:0
+aconnect 24:0 130:0
+#aconnect 128:0 130:0
+aconnect 131:0 130:0
+aconnect 131:0 129:0
